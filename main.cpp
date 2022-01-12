3927dc7d99c5c16667880e4e98034bac2d244942
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2022-01-12 18:13:13 +0100

    Adding comment

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
-#define CATCH_CONFIG_MAIN
+/*
+
+https://www.youtube.com/watch?v=Ob5_XZrFQH0
+by Phil Nash
+*/
+
+#define CATCH_CONFIG_MAIN //writes main for us
 
 #include "catch.hpp"
 #include "tests.cpp"
