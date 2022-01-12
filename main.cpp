9ae01709a5696a03e9acadf7e9b2f271aa872dfa
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2022-01-12 18:22:42 +0100

    Adding comment

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 /*
-
+catch2 repository
+https://github.com/catchorg/Catch2
+and introduction
 https://www.youtube.com/watch?v=Ob5_XZrFQH0
-by Phil Nash
-*/
+by Phil Nash, cppcon 2018
+*/ 
 
 #define CATCH_CONFIG_MAIN //writes main for us
 
