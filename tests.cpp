c3b4178cdb145f3554a81d6e803d6c9aa6f06799
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2022-01-12 17:01:03 +0100

    matchers tests

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,17 @@
 #include "catch.hpp"
 
-TEST_CASE(){
-    REQUIRE(6*9 == 42);
+TEST_CASE("test case name"){
+    REQUIRE(6*9 == 42); //fails
+}
+
+inline const char* testStringForMatching(){
+    return "this string contains 'abc' as a substring";
+}
+using namespace Catch::Matchers;
+TEST_CASE("String matchers", "[matchers]"){
+    REQUIRE_THAT( testStringForMatching(), Contains( "string" ) );
+    CHECK_THAT  ( testStringForMatching(), Contains( "abc"    ) );
+
+    CHECK_THAT(   testStringForMatching(), StartsWith( "this"      ) );
+    CHECK_THAT(   testStringForMatching(), EndsWith  ( "substring" ) );
 }
