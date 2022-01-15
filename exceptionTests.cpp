86a76f68cbacbe36d7ee38c9aa7bcbe575c3b710
oliver_lueken@gmx.de <oliver_lueken@gmx.de>
2022-01-15 12:04:22 +0100

    Adding exception basics

--- exceptionTests.cpp
+++ exceptionTests.cpp
@@ -2,6 +2,64 @@
 #include <string>
 #include <sstream>
 
+/*
+//Expects no exceptions thrown
+REQUIRE_NOTHROW(expression)
+  CHECK_NOTHROW(expression)
+
+
+//Expects exceptions to be thrown
+REQUIRE_THROWS(expression)
+  CHECK_THROWS(expression)
+
+
+//Expects exceptions of the specified type to be thrown
+REQUIRE_THROWS_AS(expression, exception_type)
+  CHECK_THROWS_AS(expression, exception_type)
+
+
+//Expects exceptions that, converted to a string, matches the string or string matcher
+REQUIRE_THROWS_WITH(expression, string or string matcher)
+  CHECK_THROWS_WITH(expression, string or string matcher)
+*/
+
+#include <stdexcept>
+
+auto notThrowing(){}
+auto throwing(){ throw "exception"; }
+
+TEST_CASE("not throwing"){
+    REQUIRE_NOTHROW( notThrowing() );
+    CHECK_NOTHROW( notThrowing() );
+    CHECK_NOTHROW( throwing() ); //fails
+}
+
+
+TEST_CASE("throwing"){
+    REQUIRE_THROWS( throwing() );
+    CHECK_THROWS( throwing() );
+    CHECK_THROWS( notThrowing() ); //fails
+
+    using int_pair = std::pair<int,int>;
+    CHECK_THROWS_AS( int_pair(1, 3), std::invalid_argument);
+
+    REQUIRE_THROWS_WITH( throwing(), "exception");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //My own exception
 class SpecialException : public std::exception{
 public:
