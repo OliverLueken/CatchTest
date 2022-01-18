11a00abce3807aa08115f79936101c3bfe68b827
Oliver Lüken <oliver.lueken@pm.me>
2022-01-18 16:44:55 +0100

    Adding templated fixture examples

--- fixture.cpp
+++ fixture.cpp
@@ -4,6 +4,8 @@
 
 /*
 TEST_CASE_METHOD( fixture, description, tags )
+TEMPLATE_TEST_CASE_METHOD( fixture, description, tags, type1...)
+TEMPLATE_PRODUCT_TEST_CASE_METHOD( fixture, description, tags, (templatedClass1...), (templateType1...))
 */
 
 class DBConnection{
@@ -31,9 +33,56 @@ class UniqueTestsFixture {
 
  int UniqueTestsFixture::uniqueID = 0;
 
- TEST_CASE_METHOD(UniqueTestsFixture, "Create Employee/No Name", "[create]") {
+ TEST_CASE_METHOD(UniqueTestsFixture, "Create Employee/No Name", "[create][fixture]") {
    REQUIRE_THROWS(conn.executeSQL("INSERT INTO employee (id, name) VALUES (?, ?)", getID(), ""));
  }
- TEST_CASE_METHOD(UniqueTestsFixture, "Create Employee/Normal", "[create]") {
+ TEST_CASE_METHOD(UniqueTestsFixture, "Create Employee/Normal", "[create][fixture]") {
    REQUIRE(conn.executeSQL("INSERT INTO employee (id, name) VALUES (?, ?)", getID(), "Joe Bloggs"));
  }
+
+
+
+
+ template< typename T >
+ struct Template_Fixture {
+     Template_Fixture(): m_a(1) {}
+
+     T m_a;
+ };
+
+/*
+executes test with
+TestType = int,
+TestType = float,
+TestType = double,
+*/
+ TEMPLATE_TEST_CASE_METHOD(Template_Fixture,"A TEMPLATE_TEST_CASE_METHOD based test run that succeeds", "[class][template][fixture]",
+        int, float, double) {
+     REQUIRE( Template_Fixture<TestType>::m_a == 1 );
+ }
+
+ template<typename T>
+ struct Template_Template_Fixture {
+     Template_Template_Fixture() {}
+
+     T m_a;
+ };
+
+ template<typename T>
+ struct Foo_class {
+     size_t size() {
+         return 0;
+     }
+ };
+
+/*
+executes test with
+TestType = Foo_class<int>
+TestType = std::vector<int>
+*/
+ TEMPLATE_PRODUCT_TEST_CASE_METHOD(Template_Template_Fixture, "A TEMPLATE_PRODUCT_TEST_CASE_METHOD based test succeeds", "[class][template][fixture]",
+        (Foo_class, std::vector), int) {
+     REQUIRE( Template_Template_Fixture<TestType>::m_a.size() == 0 );
+ }
+
+
