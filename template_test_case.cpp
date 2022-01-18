266418b003ddaeb9249140d63543804616d1141e
Oliver Lüken <oliver.lueken@pm.me>
2022-01-18 16:06:44 +0100

    Adding template signature examples

--- template_test_case.cpp
+++ template_test_case.cpp
@@ -6,7 +6,9 @@
 /*
 TEMPLATE_TEST_CASE( description, tags, types...)
 TEMPLATE_PRODUCT_TEST_CASE( description, tags, (templatedClass1, templatedClass2...), (templateType1, templateType2...))
-TEMPLATE_LIST_TEST_CASE( descpiption, tags, type list ) //allows reuse of the type list
+TEMPLATE_LIST_TEST_CASE( description, tags, type list ) //allows reuse of the type list
+TEMPLATE_TEST_CASE_SIG( description , tags, signature, type1, type2... )
+TEMPLATE_PRODUCT_TEST_CASE_SIG( description , tags, signature, (templatedClass1...), (templateType1...) )
 */
 
 
@@ -92,4 +94,39 @@ using MyTypes = std::tuple<int, char, float>;
 TEMPLATE_LIST_TEST_CASE("Template test case with test types specified inside std::tuple", "[template][list]", MyTypes)
 {
     REQUIRE(sizeof(TestType) > 0);
+}
+
+/*
+executes test case with
+T = int,                    V = 5
+T = float,                  V = 4
+T = std::string,            V = 15
+T = std::tuple<int, flaot>, V = 6
+*/
+TEMPLATE_TEST_CASE_SIG("TemplateTestSig: arrays can be created from NTTP arguments", "[vector][template][nttp]",
+  ((typename T, int V), T, V), (int,5), (float,4), (std::string,15), ((std::tuple<int, float>), 6)) {
+
+    std::array<T, V> v;
+    REQUIRE(v.size() > 1);
+}
+
+
+
+template<typename T, size_t S>
+struct Bar {
+    size_t size() { return S; }
+};
+
+
+/*
+executes test case with
+TestType = std::array<int, 9>
+TestType = std::array<float, 42>
+TestType = Bar<int, 9>
+TestType = Bar<float, 42>
+*/
+TEMPLATE_PRODUCT_TEST_CASE_SIG("A Template product test case with array signature", "[template][product][nttp]",
+        ((typename T, size_t S), T, S), (std::array, Bar), ((int, 9), (float, 42))) {
+    TestType x;
+    REQUIRE(x.size() > 0);
 }
