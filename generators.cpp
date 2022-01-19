445765a951ffe420bcee1633a3f3c5fbd27f021f
Oliver Lüken <oliver.lueken@pm.me>
2022-01-19 14:05:09 +0100

    Adding another generators example

--- generators.cpp
+++ generators.cpp
@@ -1,5 +1,11 @@
 #include "catch.hpp"
 
+/*
+GENERATE( val1, val2... )
+//runs test case once for each value
+*/
+
+
 TEST_CASE( "10x10 ints" ){
     auto x = GENERATE( range(1,11) );
     auto y = GENERATE( range(101,111) );
@@ -20,5 +26,29 @@ TEST_CASE("Generators"){
         auto j = GENERATE(3.141, 1.379);
         CAPTURE(i,j);
         SUCCEED();
+    }
+}
+
+/*
+i=1, j=-3
+i=1, j=-2
+i=1, k=4
+i=1, k=5
+i=1, k=6
+i=2, j=-3
+i=2, j=-2
+i=2, k=4
+i=2, k=5
+i=2, k=6
+*/
+TEST_CASE("Generators") {
+    auto i = GENERATE(1, 2);
+    SECTION("one") {
+        auto j = GENERATE(-3, -2);
+        REQUIRE(j < i);
+    }
+    SECTION("two") {
+        auto k = GENERATE(4, 5, 6);
+        REQUIRE(i != k);
     }
 }
