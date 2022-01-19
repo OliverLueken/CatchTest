de8f81852dd4cd01f479ec96ffc6a69d4ec5b89d
Oliver Lüken <oliver.lueken@pm.me>
2022-01-19 16:36:34 +0100

    Adding nofail macro

--- macros.cpp
+++ macros.cpp
@@ -3,6 +3,9 @@
 /*
 CHECKED_IF( expr ) { ... }      //executes block if expr is true
 CHECKED_ELSE( expr ) { ... }    //executes block if expr is false
+
+CHECK_NOFAIL( expr )    //CHECK that doesn't fail if expr is false
+SUCCEED( msg )
 */
 
 
@@ -11,9 +14,16 @@ TEST_CASE( "CHECKED", "[checked][macro]"){
     int b = 2;
     CHECKED_IF( a == b ) {
         // This block is entered when a == b
-        SUCCEED( "a is equal to b" );
+        SUCCEED( a << " is equal to " << b );
     } CHECKED_ELSE ( a == b ) {
         // This block is entered when a != b
-        SUCCEED( "a is not equal to b");
+        SUCCEED( a << " is not equal to " << b);
     }
+}
+
+
+
+TEST_CASE( "CHECK_NOFAIL", "[macro]"){
+    CHECK_NOFAIL( 1==2 );
+    SUCCEED("is ok");
 }
