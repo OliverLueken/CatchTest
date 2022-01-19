e06c4848f415ea273122e7a92805bb8e08729ff6
Oliver Lüken <oliver.lueken@pm.me>
2022-01-19 16:39:49 +0100

    Adding static check macro example

--- macros.cpp
+++ macros.cpp
@@ -6,6 +6,10 @@ CHECKED_ELSE( expr ) { ... }    //executes block if expr is false
 
 CHECK_NOFAIL( expr )    //CHECK that doesn't fail if expr is false
 SUCCEED( msg )
+
+STATIC_REQUIRE  //checks at compile time
+STATIC_CHECK
+CATCH_CONFIG_RUNTIME_STATIC_REQUIRE     //define to deferre check to runtime
 */
 
 
@@ -26,4 +30,10 @@ TEST_CASE( "CHECKED", "[checked][macro]"){
 TEST_CASE( "CHECK_NOFAIL", "[macro]"){
     CHECK_NOFAIL( 1==2 );
     SUCCEED("is ok");
+}
+
+
+TEST_CASE("STATIC_REQUIRE showcase", "[traits][macro]") {
+    STATIC_REQUIRE( std::is_void<void>::value );
+    STATIC_REQUIRE_FALSE( std::is_void<int>::value );
 }
